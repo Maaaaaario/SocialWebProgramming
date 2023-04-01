06f88e8204927d7593cd8c000b1512f2734002b0
Qihang Yin <353909799@qq.com>
2023-04-01 19:52:58 +0800

    movie

---
+++ src/main/java/web/Movie.js
@@ -0,0 +1,16 @@
+
+// document.querySelector("div").addEventListener("mouseover", alert("1111"));
+var e = document.getElementById("s1");
+alert(e);
+    // .addEventListener("click", alert("sss"));
+// back.addEventListener("click", goBack);
+
+// function goBack() {
+//     alert("aaa");
+//     // var player = document.querySelector('video');
+//     // if (player.src === "Movie%20Trailers/fastandfurious-trailer.mov") {
+//     //     player.src("Movie%20Trailers/Godfather-trailer.mov");
+//     //     player.load();
+//     // }
+// }
+
